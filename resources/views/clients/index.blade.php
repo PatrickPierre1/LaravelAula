45220e50e94a4a7a7ae34bcbaa2050c67d388d05
Patrick <patrickpierre2015@gmail.com>
2024-09-03 22:02:20 -0300

    create client

---
+++ resources/views/clients/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Clientes</title>
+</head>
+<body>
+    {{dd($clients[0]->nome)}}
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\SiteController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', [UserController::class, 'index']);
 
-Route::get('/produto', [SiteController::class, 'index']);
+Route::get('/clientes', [ClientController::class, 'index']);
 
 Route::get('/', function () {
     return view('welcome');
